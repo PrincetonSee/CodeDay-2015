e2df9a4b2835699a34f5a4e1b103341585dd5f49
PrincetonSee <Prince@see-empire.com>
2015-02-15 01:08:55 -0800

    Added MessageLister and Updated Server
    With these two added to the IMS folder, everyone should be able to
    create their own server that allows multiple people to chat

--- IMS/src/IMSBuild/MessageListener.java
+++ IMS/src/IMSBuild/MessageListener.java
@@ -1,13 +1,51 @@
 package IMSBuild;
 
-public class MessageListener {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MessageListener implements Runnable{
+	private Server server;
+	private Socket connection;//this is the actual connection
+	
+	public MessageListener(Server s, Socket c)
+	{
+		server = s;
+		connection = c;
 	}
-
+	
+	@Override
+	public void run() {
+		ObjectInputStream in;//this is how the server receives info
+		try {
+			in = new ObjectInputStream(connection.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			server.showMessage("\nThe connection ended from ML" + e.toString());
+			return;
+		}//this sets up your end of that "pipeline".
+		server.showMessage("\nStream setup is now made! \n");
+		server.showMessage("Chat Rules:\n\t+Your username MUST BE your real name\n\t+Do Not Troll, Spam, swear exessively or basically be an asshat");
+		String message = "Your now connected!";
+		String clientName ="CLIENT";
+		do{//this uses a rare do-while (no one really uses them) but because we need to find out when to end it. This occurs via input.
+			try{
+				message = (String) in.readObject();//this will also sit here and not do anything else until an input comes through
+				Scanner temp = new Scanner(message);//I had to include a scanner to differentiate the user-name and the message.
+				temp.useDelimiter(" - ");
+				clientName=temp.next();
+				temp.close();
+				server.showMessage("\n"+message);
+				server.sendToOthers(message, connection);
+			}catch(ClassNotFoundException classNotFoundException){
+				server.showMessage("\nError receiving message from ML! :(");
+			} catch (IOException e) {
+				server.showMessage("\nThe connection ended from ML, ERROR COMES FROM HERE" + e.toString());
+				return;
+			}
+		}while(!message.equals(clientName+" - END"));
+	}
 }
--- IMS/src/IMSBuild/Server.java
+++ IMS/src/IMSBuild/Server.java
@@ -1,7 +1,10 @@
+//Edited by Princeton See
 package IMSBuild;
 
 import java.io.*;//an import with a "*" means "all of it". 
 import java.net.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.awt.event.*;
 import java.awt.*;
@@ -15,13 +18,17 @@ public class Server extends JFrame{
 	private JTextField text;//the bottom text field
 	private JTextField userName;//the top text field
 	private JTextArea chatBox;//the text area
-	private ObjectOutputStream out;//this is how the server side sends out info
 	private ObjectInputStream in;//this is how the server receives info
 	private ServerSocket server;//this is the socket or port it will use
-	private Socket connection;//this is the actual connection
-	
+	private LinkedList<Thread> threadList;
+	private LinkedList<Socket> connectionList;
+	private HashMap<Socket, ObjectOutputStream> outputMap;
+
 	public Server(int PORT){//since this is the server version, the constructors parameter is only a port, and not an IP.
 		super("ClassChat : Host");
+		threadList = new LinkedList<Thread>();
+		connectionList = new LinkedList<Socket>();
+		outputMap = new HashMap<Socket, ObjectOutputStream>();
 		port=PORT;
 		userName = new JTextField("User name",10);
 		userName.setEditable(true);
@@ -29,11 +36,10 @@ public class Server extends JFrame{
 		text.setEditable(false);
 		text.addActionListener(new ActionListener(){//if you don't know actionListners, itemListeners, focusListners, mouseListeners... look them up, they're useful
 			public void actionPerformed(ActionEvent event){
-				sendMessage(event.getActionCommand());
 				text.setText("");
 			}
 		}
-		);
+				);
 		add(userName, BorderLayout.NORTH);
 		add(text, BorderLayout.SOUTH);
 		chatBox = new JTextArea();
@@ -42,7 +48,7 @@ public class Server extends JFrame{
 		setSize(400,250);
 		setVisible(true);
 	}
-	
+
 	public void starting(){//after the constructor, the test class calls this method to actually get it running
 		try{
 			server = new ServerSocket(port, 1004);
@@ -60,8 +66,6 @@ public class Server extends JFrame{
 				try{
 					showMessage("IP Address: "+InetAddress.getLocalHost().getHostAddress()+"\nPort: "+portString+"\n");//this is for reference
 					findConnection();
-					setStreams();
-					whileChat();
 				}catch(EOFException eofException){
 					showMessage("\nThe connection ended");
 				}finally{
@@ -74,76 +78,69 @@ public class Server extends JFrame{
 	}
 	private void findConnection()throws IOException{
 		showMessage("\nWaiting for connection . . .");
-		connection = server.accept();//this will sit here until it gets a connection made.
-		showMessage("\nConnected to "+connection.getInetAddress().getHostName());
-	}
-	private void setStreams()throws IOException{
-		out = new ObjectOutputStream(connection.getOutputStream());//this pretty much cleans a "pipeline" and gets it ready for use
-		out.flush();
-		in = new ObjectInputStream(connection.getInputStream());//this sets up your end of that "pipeline".
-		showMessage("\nStream setup is now made! \n");
-	}
-	private void whileChat()throws IOException{
-		showMessage("Chat Rules:\n\t+Your username MUST BE your real name\n\t+Do Not Troll, Spam, swear exessively or basically be an asshat");
-		String message = "Your now connected!";
-		sendMessage(message);
-		String clientName ="CLIENT";
-		type(true);
-		do{//this uses a rare do-while (no one really uses them) but because we need to find out when to end it. This occurs via input.
-			try{
-				message = (String) in.readObject();//this will also sit here and not do anything else until an input comes through
-				Scanner temp = new Scanner(message);//I had to include a scanner to differentiate the user-name and the message.
-				temp.useDelimiter(" - ");
-				clientName=temp.next();
-				temp.close();
-				showMessage("\n"+message);
-			}catch(ClassNotFoundException classNotFoundException){
-				showMessage("\nError receiving message! :(");
-			}
-		}while(!message.equals(clientName+" - END"));
+		while(1 != 2)
+		{
+			Socket connection = server.accept();//this will sit here until it gets a connection made.
+			connectionList.add(connection);
+			outputMap.put(connection, new ObjectOutputStream(connection.getOutputStream()));
+			send(connection, "You Are Now Connected!");
+			showMessage("\nConnected to "+connection.getInetAddress().getHostName());
+			Thread t = new Thread(new MessageListener(this, connection));
+			t.start();
+			threadList.add(t);
+		}
 	}
 	private void close(){//a simple method to close things up
 		showMessage("\nClosing connections...\n");
 		type(false);
 		try{
-			out.close();
 			in.close();
-			connection.close();
+			for(Socket s : connectionList)
+			{
+				s.close();
+			}
 		}catch(IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
-	private void sendMessage(String message){//this is how to send messages
-		try{
-			if(message.equals("Your now connected!")){//this is if you are first connected- i did this little phrase because there
-				out.writeObject("Your now connected to the host!");//is not my user-name in there. this message will only occur once
-				out.flush();
-				showMessage("\nClient connected!");
-			}else{
-			out.writeObject(userName.getText()+" - " + message);//the message is sent out
-			out.flush();//the message is "flushed" which is really just pushing all bits through like a pump. It wraps it up.
-			showMessage("\n"+userName.getText()+" - "+ message);
-			}
-		}catch(IOException ioException){
-			chatBox.append("\n Error sending message! :(");
-		}
-	}
-	private void showMessage(final String text){
+	public synchronized void showMessage(final String text){
 		SwingUtilities.invokeLater(
-			new Runnable(){
-				public void run(){
-					chatBox.append(text);//this adds text to the text area
-				}
-			}	
-		);
+				new Runnable(){
+					public void run(){
+						chatBox.append(text);//this adds text to the text area
+					}
+				}	
+				);
 	}
 	private void type(final boolean ToF){
 		SwingUtilities.invokeLater(
-			new Runnable(){
-				public void run(){
-					text.setEditable(ToF);//this is what permits you to type or not.
-				}
-			}	
-		);
+				new Runnable(){
+					public void run(){
+						text.setEditable(ToF);//this is what permits you to type or not.
+					}
+				}	
+				);
+	}
+
+	public void sendToOthers(final String text, Socket soc)
+	{
+		for(Socket s : connectionList)
+		{
+			if(s != soc)
+			{
+				send(s, text);
+			}
+		}
+
+	}
+
+	public void send(Socket soc, String text)
+	{
+		try {
+			outputMap.get(soc).writeObject(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
